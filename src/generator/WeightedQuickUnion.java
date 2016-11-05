89a4caa1db9eafcf73f7c4621dc7a0e5c1a7f48c
cjdouglas <chris.douglas.210@gmail.com>
2016-11-05 18:18:41 -0500

    Added WeightedQuickUnion structure

---
+++ src/generator/WeightedQuickUnion.java
@@ -0,0 +1,70 @@
+package generator;
+
+public class WeightedQuickUnion {
+	private int[] id;
+	private int[] size;
+	
+	/**
+	 * Initializer for WeightedQuickUnion with a given number of elements
+	 * @param N The width and height of the maze to generate, N x N
+	 */
+	public WeightedQuickUnion(int N) {
+		id = new int[N * N];
+		size = new int[N * N];
+
+		// Initialize each square with its own unique id and size of 1
+		
+		for (int i = 0; i < N * N; i++) {
+			id[i] = i;
+			size[i] = 1;
+		}
+	}
+	
+	/**
+	 * Returns the index of the root for the given node index while
+	 * redirecting all child node's to point to the parent for faster access
+	 * @param i The index of the node to find the root for
+	 * @return The index of the root for the given node index
+	 */
+	private int root(int i) {
+		while (i != id[i]) {
+			id[i] = id[id[i]];
+			i = id[i];
+		}
+		
+		return i;
+	}
+	
+	/**
+	 * Combines the connect components for the given nodes by attaching
+	 * the smaller tree as a child of the larger tree's parent
+	 * @param p Index of the first node
+	 * @param q Index of the second node
+	 */
+	public void union(int firstNode, int secondNode) {
+		int i = root(firstNode);
+		int j = root(secondNode);
+		
+		if (i == j) {
+			return; // Return if they are already in the same connected component
+		}
+		
+		if (size[i] < size[j]) {
+			id[i] = id[j];
+			size[j] += size[i];
+		} else {
+			id[j] = id[i];
+			size[i] += size[j];
+		}
+	}
+	
+	/**
+	 * Returns true/false whether two nodes are in the same connected component
+	 * @param p Index of the first node
+	 * @param q Index of the second node
+	 * @return True if the two nodes are in the same connected component, false otherwise
+	 */
+	public boolean connected(int firstNode, int secondNode) {
+		return id[firstNode] == id[secondNode];
+	}
+}
