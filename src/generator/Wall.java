ca8e86547283290d2b5452e4e5cc272721dfc762
cjdouglas <chris.douglas.210@gmail.com>
2016-11-05 23:30:09 -0500

    Renamed Edge class to Wall
    - Added private modifiers to variables inside Wall

---
+++ src/generator/Wall.java
@@ -0,0 +1,32 @@
+package generator;
+
+public class Wall {
+	private int x;
+	private int y;
+	
+	/**
+	 * Initializer for a Wall
+	 * @param x The first node index associated with this Wall
+	 * @param y The second node index associated with this Wall
+	 */
+	public Wall(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Returns the first node of this Wall
+	 * @return The first node of this Wall
+	 */
+	public int x() {
+		return x;
+	}
+	
+	/**
+	 * Returns the second node of this wall
+	 * @return The second node of this wall
+	 */
+	public int y() {
+		return y;
+	}
+}
